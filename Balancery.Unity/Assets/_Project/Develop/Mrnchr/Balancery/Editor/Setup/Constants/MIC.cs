400fc4d38993ec9bebf0963e09e586190d1f18ad
Subbota Mikhail <mihail.subbota@inbox.ru>
2025-05-05 14:38:22 +0300

    feat: Add methods to switch project define

---
+++ Balancery.Unity/Assets/_Project/Develop/Mrnchr/Balancery/Editor/Setup/Constants/MIC.cs
@@ -0,0 +1,11 @@
+﻿namespace Mrnchr.Balancery.Editor.Setup
+{
+  /// <summary>
+  /// Menu Item Constants
+  /// </summary>
+  public static class MIC
+  {
+    public const string TOOLS_MENU = "Tools/";
+    public const string PROJECT_TOOLS_MENU = TOOLS_MENU + "Balancery/";
+  }
+}
